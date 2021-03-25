dde65ebbef79fe6e0bca522c869a9afda53c9e56
agu18044 <78278462+agu18044@users.noreply.github.com>
2021-03-24 21:00:21 -0600

    se agrego menu para selecionar

--- Lab5/Lab5.ino
+++ Lab5/Lab5.ino
@@ -21,6 +21,7 @@
 #include <SPI.h>
 #include <SD.h>
 
+File root;
 File myFile;
 
 void setup()
@@ -45,23 +46,14 @@ void setup()
   } 
   Serial.println("initialization done.");
 
-  // open the file. note that only one file can be open at a time,
-  // so you have to close this one before opening another.
-  myFile = SD.open("test.txt", FILE_WRITE);
+  root = SD.open("/");
 
-  // if the file opened okay, write to it:
-  if (myFile) {
-    Serial.print("Writing to test.txt...");
-    myFile.println("Hola mundo...");
-    // close the file:
-    myFile.close();
-    Serial.println("done.");
-  } else {
-    // if the file didn't open, print an error:
-    Serial.println("error opening test.txt");
-  }
+  printDirectory(root, 0);
 
-  // re-open the file for reading:
+  Serial.println("done!");
+
+
+  // open the file for reading:
   myFile = SD.open("test.txt");
   if (myFile) {
     Serial.println("test.txt:");
@@ -80,5 +72,28 @@ void setup()
 
 void loop()
 {
-  // nothing happens after setup
+  
+}
+void printDirectory(File dir, int numTabs) {
+   while(true) {
+     
+     File entry =  dir.openNextFile();
+     if (! entry) {
+       // no more files
+       break;
+     }
+     for (uint8_t i=0; i<numTabs; i++) {
+       Serial.print('\t');
+     }
+     Serial.print(entry.name());
+     if (entry.isDirectory()) {
+       Serial.println("/");
+       printDirectory(entry, numTabs+1);
+     } else {
+       // files have sizes, directories do not
+       Serial.print("\t\t");
+       Serial.println(entry.size(), DEC);
+     }
+     entry.close();
+   }
 }
