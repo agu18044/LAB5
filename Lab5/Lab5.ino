b525d110afaee918451c95a6d50b407b36c95ea6
agu18044 <78278462+agu18044@users.noreply.github.com>
2021-03-25 11:28:55 -0600

    agrego codigo para poder elegir que archivo abrir

--- Lab5/Lab5.ino
+++ Lab5/Lab5.ino
@@ -26,6 +26,8 @@ File root;
 File myFile;
 
 void printfile1 (void);
+void printfile2 (void);
+void printfile3 (void);
 
 void setup(){
   // Open serial communications and wait for port to open:
@@ -62,6 +64,12 @@ void loop(){
   if (inByte =='1'){
    printfile1();
   }
+  if (inByte =='2'){
+   printfile2();
+  }
+  if (inByte =='3'){
+   printfile3();
+  }
   
 }
 void printDirectory(File dir, int numTabs) {
@@ -89,9 +97,9 @@ void printDirectory(File dir, int numTabs) {
 }
 
 void printfile1 (void){
-    myFile = SD.open("test.txt");
+    myFile = SD.open("kirby.txt");
   if (myFile) {
-    Serial.println("test.txt:");
+    Serial.println("Kirby:");
 
     // read from the file until there's nothing else in it:
     while (myFile.available()) {
@@ -101,6 +109,43 @@ void printfile1 (void){
     myFile.close();
   } else {
     // if the file didn't open, print an error:
-    Serial.println("error opening test.txt");
+    Serial.println("error opening file");
   }
+  Serial.println(" ");
+  }
+  
+void printfile2 (void){
+    myFile = SD.open("star.txt");
+  if (myFile) {
+    Serial.println("Star:");
+
+    // read from the file until there's nothing else in it:
+    while (myFile.available()) {
+      Serial.write(myFile.read());
+    }
+    // close the file:
+    myFile.close();
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening file");
+  }
+  Serial.println(" ");
+  }
+  
+void printfile3 (void){
+    myFile = SD.open("goomba.txt");
+  if (myFile) {
+    Serial.println("Goomba:");
+
+    // read from the file until there's nothing else in it:
+    while (myFile.available()) {
+      Serial.write(myFile.read());
+    }
+    // close the file:
+    myFile.close();
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening file");
+  }
+  Serial.println(" ");
   }
